02950a1f7a97a242d5b9492a6e7d5316a23ac902
20192726 <20192726@fue.edu.eg>
2023-11-27 14:05:32 +0200

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         navigatorKey: AppService().navigatorKey,
         onGenerateRoute:RouteGenerator.getRoute,
         title: AppStrings.tripia,
-        home: const validator(),
+        //home: const validator(),
       ),
     );
   }
